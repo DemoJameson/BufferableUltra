4c9b256bee5a181257689dfaa492ed74e670bcf1
DemoJameson <demojameson@gmail.com>
2020-12-18 11:03:10 +0800

    Added GroundOnly and AvoidAffectingTAS options.

--- BufferableUltraModule.cs
+++ BufferableUltraModule.cs
@@ -7,10 +7,12 @@ namespace Celeste.Mod.BufferableUltra {
         public static BufferableUltraSettings Settings => (BufferableUltraSettings) Instance._Settings;
 
         private static readonly FieldInfo OnGround = typeof(Player).GetField("onGround", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly Lazy<FieldInfo> tasManagerRunning =
+            new Lazy<FieldInfo>(() => Type.GetType("TAS.Manager, CelesteTAS-EverestInterop")?.GetField("Running", BindingFlags.Static | BindingFlags.Public));
+
         public override Type SettingsType => typeof(BufferableUltraSettings);
 
         private bool climbJump = false;
-        private readonly Version stableVersion = new Version(1, 3, 1, 2);
 
         public BufferableUltraModule() {
             Instance = this;
@@ -33,8 +35,15 @@ namespace Celeste.Mod.BufferableUltra {
         }
 
         private void PlayerOnJump(On.Celeste.Player.orig_Jump orig, Player player, bool particles, bool playSfx) {
-            if (Celeste.Instance.Version == stableVersion && Settings.Enabled && !climbJump && player.DashDir.X != 0f && player.DashDir.Y > 0f &&
-                player.Speed.Y > 0f && (bool) OnGround.GetValue(player)) {
+            if (!Settings.Enabled ||
+                Settings.GroundOnly && (bool) OnGround.GetValue(player) == false ||
+                Settings.AvoidAffectingTAS && (bool?) tasManagerRunning.Value?.GetValue(null) == true
+            ) {
+                orig(player, particles, playSfx);
+                return;
+            }
+
+            if (!climbJump && player.DashDir.X != 0f && player.DashDir.Y > 0f && player.Speed.Y > 0f) {
                 player.DashDir.X = Math.Sign(player.DashDir.X);
                 player.DashDir.Y = 0f;
                 player.Ducking = true;
--- BufferableUltraSettings.cs
+++ BufferableUltraSettings.cs
@@ -1,5 +1,7 @@
 ﻿namespace Celeste.Mod.BufferableUltra {
     public class BufferableUltraSettings : EverestModuleSettings {
         public bool Enabled { get; set; } = true;
+        public bool GroundOnly { get; set; } = true;
+        public bool AvoidAffectingTAS { get; set; } = true;
     }
 }
